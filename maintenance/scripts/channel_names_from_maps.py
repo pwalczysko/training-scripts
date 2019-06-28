4b153f134285b967b790ef14353a65b89b7ee152
Petr Walczysko <pwalczysko@bs221.biolsci.dundee.ac.uk>
2019-06-28 17:15:20 +0100

    to delete

--- maintenance/scripts/channel_names_from_maps.py
+++ maintenance/scripts/channel_names_from_maps.py
@@ -2,7 +2,7 @@
 #  Copyright (C) 2018 University of Dundee. All rights reserved.
 #
 #
-#  This program is free software; you can redistribute it and/or modify
+#   asdf safasfThis program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
